b382ccf8b644f5f3d711b8dd0395acaa20f7dc43
Jean-Philippe Bossuat <jeanphilippe.bossuat@gmail.com>
2025-04-22 11:01:48 +0200

    removed RNS backend (archived in branch archive_rns)

--- rns/src/dft.rs
+++
@@ -1,8 +0,0 @@
-pub mod ntt;
-
-pub trait DFT<O> {
-    fn forward_inplace_lazy(&self, x: &mut [O]);
-    fn forward_inplace(&self, x: &mut [O]);
-    fn backward_inplace_lazy(&self, x: &mut [O]);
-    fn backward_inplace(&self, x: &mut [O]);
-}
